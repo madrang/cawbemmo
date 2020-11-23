206da1bf7f1526c8c35fa2cbb551fa3295d910a5
vinnyluu2 <vinnyluu2@gmail.com>
2020-11-22 20:00:47 -0700

    removed duplication

--- src/server/items/generators/worth.js
+++ src/server/items/generators/worth.js
@@ -1,9 +1,8 @@
 module.exports = {
 	generate: function (item, blueprint) {
-		if (item.originalLevel) 
-			item.worth = ~~(Math.pow(item.originalLevel, 1.5) + (Math.pow((item.quality + 1), 2) * 10));
-		else
-			item.worth = ~~(Math.pow(item.level, 1.5) + (Math.pow((item.quality + 1), 2) * 10));
+		const level = item.originalLevel || item.level;
+		
+		item.worth = ~~(Math.pow(level, 1.5) + (Math.pow((item.quality + 1), 2) * 10));
 
 		if (item.spell)
 			item.worth *= 5;
