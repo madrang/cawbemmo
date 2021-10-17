cc884db87555730e29e764e9100eb11b1114af79
Shaun <skichenbrand@gmail.com>
2021-10-17 10:08:39 +0200

    enhancement #1846

---
+++ src/server/components/particles.js
@@ -0,0 +1,16 @@
+module.exports = {
+	type: 'particles',
+
+	blueprint: null,
+
+	simplify: function (self) {
+		const { blueprint } = this;
+
+		const result = {
+			type: 'particles',
+			blueprint
+		};
+
+		return result;
+	}
+};
