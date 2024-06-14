074cb2b8553f3dc542c49d048a098df52a984a72
Vikingman <drake@popalot.net>
2024-06-13 23:13:23 -0400

    kill tue

--- src/server/config/quests/templates/questKillX.js
+++ src/server/config/quests/templates/questKillX.js
@@ -69,7 +69,7 @@ module.exports = {
 				return;
 
 			this.have++;
-			this.description = 'Kill ' + this.have + '/' + this.need + ' ' + this.mobName;
+			this.description = 'Tue ' + this.have + '/' + this.need + ' ' + this.mobName;
 
 			if (this.have >= this.need)
 				this.ready();
