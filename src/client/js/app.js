6ea3fb3dbb2283e348df4afb7d303ed02caa5f01
Marc-Andre Ferland <madrang@gmail.com>
2024-06-16 01:10:05 -0400

    Moved some dependencies into the repository to be served locally.

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -2,15 +2,15 @@ require.config({
 	baseUrl: '',
 	waitSeconds: 120,
 	paths: {
-		socket: 'https://cdnjs.cloudflare.com/ajax/libs/socket.io/4.1.3/socket.io.min',
-		jquery: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.slim.min',
-		text: 'https://cdnjs.cloudflare.com/ajax/libs/require-text/2.0.12/text.min',
+		socket: 'js/dependencies/socket.io.min',
+		jquery: 'js/dependencies/jquery.slim.min',
+		text: 'js/dependencies/text.min',
 		html: 'plugins/html',
-		css: 'https://cdnjs.cloudflare.com/ajax/libs/require-css/0.1.10/css.min',
+		css: 'js/dependencies/css.min',
 		main: 'js/main',
 		helpers: 'js/misc/helpers',
 		particles: 'plugins/pixi.particles.min',
-		howler: 'https://cdnjs.cloudflare.com/ajax/libs/howler/2.1.3/howler.core.min',
+		howler: 'js/dependencies/howler.core.min',
 		longPress: 'plugins/long.press.min'
 	},
 	shim: {
