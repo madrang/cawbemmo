d126a8ebf66a9e8dab62a9a07a56e20654c44f94
Vikingman <drake@popalot.net>
2024-06-24 23:38:15 -0400

    crafting table update

--- src/server/config/recipes/table.js
+++ src/server/config/recipes/table.js
@@ -1,82 +1,97 @@
 const baseRecipes = {
 	joint: {
-		description: "Un bon vieux joint"
-		, item: {
+		item: {
 			name: "Joint"
 			, type: "consumable"
 			, sprite: [0, 8]
-			, description: "Un bon vieux joint pour ce heal"
 			, worth: 5
 			, noSalvage: true
 			, noAugment: true
 			, uses: 1
 			, effects: [{
 				type: "gainStat"
-				, rolls: {
-					stat: "hp"
-				}
 			}]
 		}
-		, materials: [{
-			quantity: 1
-		}, {
+		, materials: [
+			{
+			name: "weed"
+			, quantity: 1
+			}
+			,{
 			name: "Papier a roulé"
 			, quantity: 1
-		}
-
-		]
-	}
-	,djoint: {
-		description: "Un deux papier"
-		, item: {
-			name: "Joint"
-			, type: "consumable"
-			, sprite: [0, 8]
-			, description: "Un gros 2 papier pour la mana"
-			, worth: 10
-			, noSalvage: true
-			, noAugment: true
-			, uses: 1
-			, effects: [{
-				type: "gainStat"
-				, rolls: {
-					stat: "mp"
-				}
-			}]
-		}
-		, materials: [{
-			quantity: 1
-		}, {
-			name: "Papier a roulé"
-			, quantity: 2
-		}
-
+			}
 		]
 	}
 };
 
-const buildRecipe = function (recipeName, itemName, effectAmount, materialName, quantity) {
-	return extend({}, baseRecipes[recipeName], {
-		name: itemName
-		, item: {
-			name: itemName
-			, quantity: quantity
-			, effects: [{
-				rolls: {
-					amount: effectAmount
-				}
-			}]
-		}
-		, materials: [{
-			name: materialName
-		}]
-	});
+const buildRecipe = function (recipe, item, materials, effects) {
+    if (!recipe.name) {
+        throw new Error("Missing recipe name.")
+    }
+    if (!item.name) {
+        throw new Error("Missing item name.")
+    }
+    if (materials && !Array.isArray(materials)) {
+        materials = [ materials ];
+    }
+    if (effects && !Array.isArray(effects)) {
+        effects = [ effects ];
+    }
+    return extend({}, baseRecipes[recipe.name], recipe, {
+        name: item.name
+        , item: {
+            effects: effects || []
+        }
+        , materials: materials || []
+    });
 };
 
 module.exports = [
 // 	(recette, "nom de l'objet", Effets, "ingrédients", Quantité crafté)
-	buildRecipe("joint", "Un bon vieux joint", 50, "weed", 1)
-	,	buildRecipe("djoint", "Un deux papier", 100, "weed", 1)
+	buildRecipe({
+		//recette
+			name: "joint"
+			, description: "Un bon vieux joint pour ce heal<br /><br />Donne: 50 Hp instant"
+		}
+		,{
+		//item
+			name: "Un bon vieux joint"
+			, description: "Un bon vieux joint pour ce heal<br /><br />Donne: 50 Hp instant"
+		}
+		, undefined
+		, {
+			rolls: {
+					stat: "hp"  //stat mp hp
+					, amount: "50" // montant de base
+				}
+		})
+	, buildRecipe({
+		//recette
+			name: "joint"
+			, description: "Un bon vieux joint pour ce heal<br /><br />Donne: 50 Mana instant"
+		}
+		,{
+		//item
+			name: "Joint Deux papier"
+			, description: "Un bon vieux joint pour ce donner de la mana<br /><br />Donne: 50 Mana instant"
+		}
+		,[{
+	 		name: "Papier a roulé"
+	 		, quantity: 2
+			}
+			, {
+			name: "weed"
+			, quantity: 2
+			}
+		], {
+			rolls: {
+					stat: "mp"  //stat mp hp
+					, amount: "50" // montant de base
+				}
+		})
+
+
 	// , buildRecipe("carp", "Big Carp on a Stick", 50, "Big Sun Carp", 2)
 	// , buildRecipe("carp", "Giant Carp on a Stick", 150, "Giant Sun Carp", 1)
 	// , buildRecipe("carp", "Trophy Carp on a Stick", 150, "Trophy Sun Carp", 2)
