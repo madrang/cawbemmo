3333544a7299fd26b1d425609f27f0b2980772f2
Marc-Andre Ferland <madrang@gmail.com>
2025-11-12 21:20:07 -0500

    Converted the admin page to ES modules.

--- src/admin/main.js
+++ src/admin/main.js
@@ -1,169 +1,140 @@
-require.config({
-	baseUrl: ""
-	, waitSeconds: 120
-	, paths: {
-		assign: "/common/assign"
-		, text: "/js/dependencies/text.min"
-		, html: "/plugins/html"
-		, css: "/js/dependencies/css.min"
-		, helpers: "/js/misc/helpers"
+import "/js/misc/helpers.js";
+
+const userslistTpl = await _.loadHTML("/admin/templates/userslist.html", { raw: true });
+const errorslistTpl = await _.loadHTML("/admin/templates/errorslist.html", { raw: true });
+
+const userLevelToString = (level) => {
+	if (!level) {
+		return "Player";
 	}
-	, shim: {
-		helpers: {
-			deps: [
-				"assign"
-			]
-			, exports: "_"
-		}
+	if (level <= 9) {
+		return "Moderator";
 	}
-});
+	if (level <= 89) {
+		return "Admin";
+	}
+	return "Owner";
+};
 
-require([
-	"helpers"
-	, "html!templates/userslist"
-	, "html!templates/errorslist"
-], function (
-	glExport
-	, userslistTpl
-	, errorslistTpl
-) {
-	const userLevelToString = (level) => {
-		if (!level) {
-			return "Player";
-		}
-		if (level <= 9) {
-			return "Moderator";
-		}
-		if (level <= 89) {
-			return "Admin";
-		}
-		return "Owner";
-	};
-	const buildErrorsList = async () => {
-		let uiElm = document.querySelector("#errors-list");
-		if (uiElm) {
-			uiElm.remove();
-		}
-		const res = await fetch("/api/rest/errors");
-		if (!res.ok) {
-			_.log.getErrors.error("Request failed! Status:", res.statusText);
-			return;
-		}
-		const errorsList = await res.json();
-		uiElm = document.createElement("div");
-		uiElm.innerHTML = errorslistTpl;
-		uiElm = uiElm.querySelector("#errors-list");
-		const errorTpl = uiElm.querySelector(".error.item");
-		for (const errEvent of errorsList) {
-			const eventDate = new Date(errEvent.key);
-			_.log.getErrors.debug("Adding error", eventDate, errEvent.value);
-			const errElm = errorTpl.cloneNode(true);
-			errElm.innerHTML = errElm.innerHTML
-				.replaceAll("{{date}}", eventDate.toLocaleString())
-				.replaceAll("{{error}}", errEvent.value.replaceAll("\u003C", "&lt;").replaceAll("\u003E", "&gt;")
-					.replaceAll("\\n", "<br>").replaceAll("\\t", "&#9;")
-				);
-			errElm.dataset.table = "errors";
-			errElm.dataset.key = errEvent.key;
-			errElm.id = "errorEntry-" + eventDate.getTime();
-			errElm.addEventListener("click", (evt) => showMenu.call(errElm, evt, "error-item"));
-			errorTpl.parentNode.appendChild(errElm);
-		}
-		if (!errorsList.length) {
-			errorTpl.parentNode.appendChild(document.createTextNode("No errors..."));
-		}
-		errorTpl.remove();
+const buildErrorsList = async () => {
+	let uiElm = document.querySelector("#errors-list");
+	if (uiElm) {
+		uiElm.remove();
+	}
+	const res = await fetch("/api/rest/errors");
+	if (!res.ok) {
+		_.log.getErrors.error("Request failed! Status:", res.statusText);
+		return;
+	}
+	const errorsList = await res.json();
+	uiElm = document.createElement("div");
+	uiElm.innerHTML = errorslistTpl;
+	uiElm = uiElm.querySelector("#errors-list");
+	const errorTpl = uiElm.querySelector(".error.item");
+	for (const errEvent of errorsList) {
+		const eventDate = new Date(errEvent.key);
+		_.log.getErrors.debug("Adding error", eventDate, errEvent.value);
+		const errElm = errorTpl.cloneNode(true);
+		errElm.innerHTML = errElm.innerHTML
+			.replaceAll("{{date}}", eventDate.toLocaleString())
+			.replaceAll("{{error}}", errEvent.value.replaceAll("\u003C", "&lt;").replaceAll("\u003E", "&gt;")
+				.replaceAll("\\n", "<br>").replaceAll("\\t", "&#9;")
+			);
+		errElm.dataset.table = "errors";
+		errElm.dataset.key = errEvent.key;
+		errElm.id = "errorEntry-" + eventDate.getTime();
+		errElm.addEventListener("click", (evt) => showMenu.call(errElm, evt, "error-item"));
+		errorTpl.parentNode.appendChild(errElm);
+	}
+	if (!errorsList.length) {
+		errorTpl.parentNode.appendChild(document.createTextNode("No errors..."));
+	}
+	errorTpl.remove();
 
-		if (uiElm.classList.contains("modal")) {
-			buildClose(uiElm, true);
-			makeElementDraggable(uiElm);
-		}
-		const container = document.getElementById("ui-container");
-		container.appendChild(uiElm);
-	};
-	const buildUsersList = async () => {
-		let uiElm = document.querySelector("#users-list");
-		if (uiElm) {
-			uiElm.remove();
-		}
-		const res = await fetch("/api/rest/users");
-		if (!res.ok) {
-			_.log.getUsers.error("Request failed! Status:", res.statusText);
-			return;
-		}
-		const users = await res.json();
-		uiElm = document.createElement("div");
-		uiElm.innerHTML = userslistTpl;
-		uiElm = uiElm.querySelector("#users-list");
-		const userTpl = uiElm.getElementsByClassName("user")[0];
-		for (const user of users) {
-			_.log.getUsers.debug("Adding user", user);
-			const userElm = userTpl.cloneNode(true);
-			userElm.innerHTML = userElm.innerHTML
-				.replaceAll("{{username}}", user.username)
-				.replaceAll("{{level}}", `[${user.level}] ${userLevelToString(user.level)}`)
-				.replaceAll("{{characters}}", JSON.stringify(user.characters));
-			userTpl.parentNode.appendChild(userElm);
-		}
-		userTpl.remove();
+	if (uiElm.classList.contains("modal")) {
+		buildClose(uiElm, true);
+		makeElementDraggable(uiElm);
+	}
+	const container = document.getElementById("ui-container");
+	container.appendChild(uiElm);
+};
 
-		if (uiElm.classList.contains("modal")) {
-			buildClose(uiElm, true);
-			makeElementDraggable(uiElm);
+const buildUsersList = async () => {
+	let uiElm = document.querySelector("#users-list");
+	if (uiElm) {
+		uiElm.remove();
+	}
+	const res = await fetch("/api/rest/users");
+	if (!res.ok) {
+		_.log.getUsers.error("Request failed! Status:", res.statusText);
+		return;
+	}
+	const users = await res.json();
+	uiElm = document.createElement("div");
+	uiElm.innerHTML = userslistTpl;
+	uiElm = uiElm.querySelector("#users-list");
+	const userTpl = uiElm.getElementsByClassName("user")[0];
+	for (const user of users) {
+		_.log.getUsers.debug("Adding user", user);
+		const userElm = userTpl.cloneNode(true);
+		userElm.innerHTML = userElm.innerHTML
+			.replaceAll("{{username}}", user.username)
+			.replaceAll("{{level}}", `[${user.level}] ${userLevelToString(user.level)}`)
+			.replaceAll("{{characters}}", JSON.stringify(user.characters));
+		userTpl.parentNode.appendChild(userElm);
+	}
+	userTpl.remove();
+
+	if (uiElm.classList.contains("modal")) {
+		buildClose(uiElm, true);
+		makeElementDraggable(uiElm);
+	}
+	const container = document.getElementById("ui-container");
+	container.appendChild(uiElm);
+};
+
+const onConnected = (accountInfo) => {
+	_.log.submitLoginForm.info("Connected", accountInfo);
+
+	const loginElm = document.getElementById("login");
+	loginElm.style.display = "none";
+
+	const userMenu = document.getElementById("profile-menu");
+	userMenu.innerText = `${userLevelToString(accountInfo.level)}/${accountInfo.username} (${accountInfo.level})`;
+
+	delete window.submitLoginForm;
+	window.buildUsersList = buildUsersList;
+	window.buildErrorsList = buildErrorsList;
+	window.showMenu = showMenu;
+	window.deleteEntry = deleteEntry;
+	window.deleteSelectedText = deleteSelectedText;
+
+	for (const elmName of [ "top-menu", "ui-container" ]) {
+		const elm = document.getElementById(elmName);
+		elm.style.display = "block";
+	}
+};
+
+async function submitLoginForm (event) {
+	const formData = new FormData(event.target.form);
+	const data = {};
+	for (const [ key, value ] of formData.entries()) {
+		data[key] = value;
+	}
+	const res = await fetch("/api/auth/login", {
+		method: "POST"
+		, headers: {
+			"Content-Type": "application/json"
 		}
-		const container = document.getElementById("ui-container");
-		container.appendChild(uiElm);
-	};
-
-	const onConnected = (accountInfo) => {
-		_.log.submitLoginForm.info("Connected", accountInfo);
-
-		const loginElm = document.getElementById("login");
-		loginElm.style.display = "none";
-
-		const userMenu = document.getElementById("profile-menu");
-		userMenu.innerText = `${userLevelToString(accountInfo.level)}/${accountInfo.username} (${accountInfo.level})`;
-
-		window.buildUsersList = buildUsersList;
-		window.buildErrorsList = buildErrorsList;
-
-		for (const elmName of [ "top-menu", "ui-container" ]) {
-			const elm = document.getElementById(elmName);
-			elm.style.display = "block";
-		}
-	};
-
-	window.submitLoginForm = async (event) => {
-		const formData = new FormData(event.target.form);
-		const data = {};
-		for (const [ key, value ] of formData.entries()) {
-			data[key] = value;
-		}
-		const res = await fetch("/api/auth/login", {
-			method: "POST"
-			, headers: {
-				"Content-Type": "application/json"
-			}
-			, body: JSON.stringify(data)
-		});
-		if (!res.ok) {
-			_.log.submitLoginForm.info("Request Status: %s - Login failed!", res.statusText);
-			return;
-		}
-		const reply = await res.json();
-		onConnected(reply.user);
-	};
-
-	// Check if user is currently authentified.
-	fetch("/api/auth/self").then(async (res) => {
-		if (res.status === 200) {
-			return onConnected(await res.json());
-		}
-		_.log.AdminPanel.info("Not connected!");
-		const loginElm = document.getElementById("login");
-		loginElm.style.display = "flex";
-	}, _.log.AdminPanel.error);
-});
+		, body: JSON.stringify(data)
+	});
+	if (!res.ok) {
+		_.log.submitLoginForm.info("Request Status: %s - Login failed!", res.statusText);
+		return;
+	}
+	const reply = await res.json();
+	onConnected(reply.user);
+};
 
 function buildClose (uiElm, onClose) {
 	const heading = uiElm.querySelector(".heading");
@@ -376,3 +347,14 @@ async function deleteSelectedText(event) {
 	_.log.deleteSelectedText.trace("Done!");
 }
 
+window.submitLoginForm = submitLoginForm;
+
+// Check if user is currently authentified.
+fetch("/api/auth/self").then(async (res) => {
+	if (res.status === 200) {
+		return onConnected(await res.json());
+	}
+	_.log.AdminPanel.info("Not connected!");
+	const loginElm = document.getElementById("login");
+	loginElm.style.display = "flex";
+}, _.log.AdminPanel.error);
